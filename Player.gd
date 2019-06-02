3df9d18f2a9947032356037d14445e23c61feaf3
은냇물 <naetmul@gmail.com>
2019-06-02 21:04:57 +0900

    Input from Keyboard to Mouse or Touch

--- Player.gd
+++ Player.gd
@@ -3,24 +3,46 @@ extends Area2D
 signal hit
 
 export var speed = 400  # How fast the player will move (pixels/sec).
+var velocity = Vector2()
 var screen_size  # size of the game window.
+# Add this variable to hold clicked position
+var target = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
+func start(pos):
+	position = pos
+	# Initial target is the start position.
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
+# Change the target whenever a touch event happens.
+func _input(event):
+	if (event is InputEventMouseButton or event is InputEventScreenTouch) and event.pressed:
+		target = event.position
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	# Move towards the target and stop when close.
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized() * speed
+	else:
+		velocity = Vector2()
+		
+# Remove keyboard controls.
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y -= 1
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -28,8 +50,10 @@ func _process(delta):
 		$AnimatedSprite.stop()
 		
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	# We don't need to clamp the player's position
+	# because you can't click outside the screen.
+	# position.x = clamp(position.x, 0, screen_size.x)
+	# position.y = clamp(position.y, 0, screen_size.y)
 	
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
@@ -44,8 +68,3 @@ func _on_Player_body_entered(body):
 	hide()  # Player disappears after being hit.
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
